63b94c6ba68124ae0c09e088b7bd483f831e4e4a
Peter Milne <milnepe@gmail.com>
2021-04-15 15:28:09 +0100

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # pi-hq-camera
 Raspberry Pi high quality camera project - capture still and video images
+
+Python3 script to control the Raspberry Pi High Quality Camera module using a push button with the following features:
+
+Three camera modes. Pressing and holding the button for 2 seconds cycles through each mode indicated by an LED:
+* Mode 1 (default) - Still capture - slow flash
+* Mode 2 Video capture - fast flash
+* Mode 3 Safe shutdown - Raspberry Pi safe power off - solid on
+
+Camera functions are controlled by short button presses:
+* Mode 1 - Still capture - press and release button to capture an image
+* Mode 2 - Video capture - press and release button to start video recording, press and release again to stop video recording
+* Mode 3 - Safe shutdown - press and release button to perform Raspberry Pi safe shutdown
+
+## Installation
+Clone the repository to your Raspberry Pi home directory:
+
+```git clone https://github.com/milnepe/pi-hq-camera.git
+```
+
+Before running the script enable the camera interface using the Raspberry Pi Configuration from the Preferences menu or raspi-config and reboot:
+
+```cd pi-hq-camera
+python3 pi-hq-camera.py
+```
+
+## Automatic start
+To run the script automatically when the Raspberry Pi boots up install the systemd unit:
+
+```sudo cp hq-camera.service /etc/systemd/system/
+sudo systemctl enable hq-camera.service
+sudo reboot
+```
+
+## Image directories
+By default images and video are saved to the following directories:
+/home/pi/Pictures
+/home/pi/Videos
+
+## Hardware
+This script has been designed to use DFRobot Gravity LED & Button modules. These modules can be safely connected directly to the GPIO header using the standard Gravity cables as below:
+**Warning - check that it is safe to connect any other type of module in this way as you may damage your Raspberry Pi**
+
+Button
+Pin | GPIO   | Func | Colour
+--- | ------ | ---- | ------
+09  | GND    | GND  | BLACK
+11  | GPIO17 | VCC  | RED
+23  | GPIO27 | CTL  | ORANGE
+
+LED
+Pin | GPIO   | Func | Colour
+--- | ------ | ---- | ------
+16  | GPIO23 | CTL  | ORANGE
+18  | GPIO24 | VCC  | RED
+20  | GND    | GND  | BLACK
---
+++ hq-camera.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python3
+
+from gpiozero import DigitalOutputDevice, LED, Button
+from signal import pause
+from subprocess import check_call, Popen
+from picamera import PiCamera
+from datetime import datetime
+from time import sleep
+
+user = "yourusername"
+remote_dir = "hostname:~/remotepath"
+image_dir = "/home/pi/Pictures/"
+video_dir = "/home/pi/Videos/"
+filename = None
+
+states = ['still', 'video', 'shutdown']
+state = states[0]
+
+Button.was_held = False
+video_state = False
+
+camera = PiCamera()
+
+# Set pin 24 HIGH
+led_power = DigitalOutputDevice(24, initial_value=True)
+# State Indicator
+led = LED(23)
+
+# Set pin 17 HIGH
+btn_power = DigitalOutputDevice(17, initial_value=True)
+# Button with external pull down resistor
+btn = Button(27, pull_up=None, active_state=True, hold_time=2)
+
+
+def take_still():
+    global filename
+    print("click...")
+    camera.stop_preview()
+    filename = datetime.now().strftime('%Y%m%dT%H%M%S')
+    # Take a shot
+    camera.capture(f'{image_dir}{filename}.jpg')
+    print(f'{image_dir}{filename}.jpg')
+
+    # copy to remote dir using secure copy
+    # Popen(f'scp {video_dir}{filename}.jpg {user}@{remote_dir}.jpg', shell=True)
+    # print(f'image transfered...')
+
+    # Re-start preview mode
+    camera.start_preview()
+
+
+def take_video():
+    global video_state
+    global filename
+    if video_state is False:
+        filename = datetime.now().strftime('%Y%m%dT%H%M%S')
+        video_state = True
+        print(f'start recording {filename}.h264')
+        # record video to tmp dir
+        camera.start_recording(f'/tmp/{filename}.h264')
+    else:
+        video_state = False
+        camera.stop_recording()
+        print("stop video...")
+        sleep(2)
+        # convert to mp4 and save in video dir
+        Popen(f'MP4Box -add /tmp/{filename}.h264 {video_dir}{filename}.mp4', shell=True)
+        print(f'Done encoding...')
+
+
+def do_shutdown():
+    print("shutdown...")
+    check_call(['sudo', 'poweroff'])
+
+
+def change_state():
+    global state
+    if state is 'still':
+        state = states[1]
+        led.blink(on_time=0.25, off_time=0.25)
+    elif state is 'video':
+        state = states[2]
+        led.on()
+    elif state is 'shutdown':
+        state = states[0]
+        led.blink()
+    print("state: ", state)
+
+
+def op():
+    global state
+    if state is 'still':
+        take_still()
+    elif state is 'video':
+        take_video()
+    elif state is 'shutdown':
+        do_shutdown()
+
+
+def held(btn):
+    btn.was_held = True
+    print("button was held not just pressed")
+    change_state()
+
+
+def released(btn):
+    if not btn.was_held:
+        pressed()
+    btn.was_held = False
+
+
+def pressed():
+    print("button was pressed not held")
+    op()
+
+
+def init_camera():
+    camera.rotation = 270
+    print("state: ", state)
+    led.blink()
+    camera.start_preview()
+
+
+init_camera()
+
+btn.when_held = held
+btn.when_released = released
+
+pause()
